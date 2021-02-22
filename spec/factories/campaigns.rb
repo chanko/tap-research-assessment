53b025e5858e67d1be25e0110e2ea951884f16e3
Cesar Camacho <ccamacho@credijusto.com>
2021-02-22 12:40:20 -0600

    Annotate campaign factory

--- spec/factories/campaigns.rb
+++ spec/factories/campaigns.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: campaigns
+#
+#  id                  :bigint           not null, primary key
+#  cpi                 :string
+#  length_of_interview :integer
+#  name                :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
 FactoryBot.define do
   factory :campaign do
     cpi { rand(1.0..20.0).round(2) }
