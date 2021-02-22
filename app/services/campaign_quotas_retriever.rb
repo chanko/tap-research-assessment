afae8122256a2582764fa58e3709b48295f42b81
Cesar Camacho <ccamacho@credijusto.com>
2021-02-22 12:44:07 -0600

    Remove persist logic from retriever class

--- app/services/campaign_quotas_retriever.rb
+++ app/services/campaign_quotas_retriever.rb
@@ -11,35 +11,6 @@ class CampaignQuotasRetriever
   def retrieve
     raw_response = connection.get("#{CAMPAIGNS_URL}/#{campaign_id}").body
     
-    @response = JSON.parse(raw_response)
-
-    persist_campaign_quotas
-
-    return self
-  end
-
-  private
-
-  def persist_campaign_qualifications(quota_id, qualifications)
-    qualifications.each do |hash|
-      CampaignQualification.create(
-        campaign_quota_id: quota_id,
-        question_id: hash['question_id'],
-        pre_codes: hash.fetch('pre_codes', []).join(',')
-      )
-    end
-  end
-
-  def persist_campaign_quotas
-    response.fetch('campaign_quotas', []).each do |hash|
-      CampaignQuota.create(
-        campaign_id: campaign_id,
-        id: hash['id'],
-        name: hash['name'],
-        num_respondents: hash['num_respondents']
-      )
-
-      persist_campaign_qualifications(hash['id'], hash['campaign_qualifications'])
-    end
+    JSON.parse(raw_response)
   end
 end
--- app/services/campaigns_retriever.rb
+++ app/services/campaigns_retriever.rb
@@ -1,7 +1,7 @@
 class CampaignsRetriever
   CAMPAIGNS_URL = '/api/v1/campaigns'
 
-  attr_reader :connection, :response
+  attr_reader :connection
 
   def initialize(connection)
     @connection = connection
@@ -10,23 +10,6 @@ class CampaignsRetriever
   def retrieve
     raw_response = connection.get(CAMPAIGNS_URL).body
 
-    @response = JSON.parse(raw_response)
-
-    persist_campaigns
-
-    return self
-  end
-
-  private
-
-  def persist_campaigns
-    response.each do |hash|
-      Campaign.create(
-        id: hash['id'],
-        cpi: hash['cpi'],
-        length_of_interview: hash['length_of_interview'],
-        name: hash['name']
-      )
-    end
+    JSON.parse(raw_response)
   end
 end
