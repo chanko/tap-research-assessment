fb74aaad031f0308ef5bb167f0268696fdff8f95
Cesar Camacho <ccamacho@credijusto.com>
2021-02-21 21:31:10 -0600

    Add service to retrieve and persiste campaigns

---
+++ spec/services/campaigns_retriever_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe CampaignsRetriever, type: :model do
+  let(:stubs) { Faraday::Adapter::Test::Stubs.new }
+  let(:connection) { Faraday.new { |b| b.adapter(:test, stubs) } }
+  let(:retriever) { CampaignsRetriever.new(connection) }
+
+  context '#retrieve' do
+    before(:each) do
+      allow(connection).to receive(:get)
+        .with('/api/v1/campaigns')
+        .and_return(
+          double('response', status: 200, body: "[{\"id\":278562,\"cpi\":\"2.0\",\"length_of_interview\":10,\"name\":\"Cereal\",\"total_remaining\":100,\"status\":6,\"supplier_link\":\"http://www.developingmedia.com/adhoc.php?id=\",\"incidence\":100,\"reentry_interval\":null,\"days_in_field\":5,\"max_daily_completes\":null,\"is_retarget\":false},{\"id\":278563,\"cpi\":\"1.25\",\"length_of_interview\":2,\"name\":\"Pizza\",\"total_remaining\":123,\"status\":6,\"supplier_link\":\"http://www.developingmedia.com/adhoc.php?id=\",\"incidence\":50,\"reentry_interval\":null,\"days_in_field\":5,\"max_daily_completes\":null,\"is_retarget\":false}]"
+          )
+      )
+    end
+
+    it 'creates 2 Campaign objects' do
+      expect{retriever.retrieve}.to change{Campaign.count}.from(0).to(2)
+      
+      stubs.verify_stubbed_calls
+    end
+  end
+end
