30496efe35181c3941a9b693f17fc3882d84007f
Cesar Camacho <ccamacho@credijusto.com>
2021-02-23 02:03:16 -0600

    Update campaign spec

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Campaign, type: :model do
   describe 'Associations' do
     it { should have_many(:campaign_quotas).class_name('CampaignQuota') }
+    it { should have_many(:campaign_qualifications).through(:campaign_quotas).class_name('CampaignQualification') }
   end
 end
