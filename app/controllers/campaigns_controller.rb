07ab6dfa453d244ee840d55eb1d6bf39fa19b2b3
Cesar Camacho <ccamacho@credijusto.com>
2021-02-23 01:41:29 -0600

    Tweak render json

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -2,8 +2,6 @@ class CampaignsController < ApplicationController
   def ordered_campaigns
     @campaigns = Campaign.includes(:campaign_quotas, :campaign_qualifications).left_outer_joins(:campaign_qualifications).group('campaigns.id').select('campaigns.*, count(*) as qualifications_count').order('qualifications_count DESC')
 
-    respond_to do |format|
-      format.json { render json: @campaigns }
-    end
+    render json: @campaigns
   end
 end
