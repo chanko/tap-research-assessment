e5dcf766c3d0067fed87b4809ad39914087f6aa2
Cesar Camacho <ccamacho@credijusto.com>
2021-02-23 02:05:19 -0600

    Call job to retrieve data in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Retrieve all data fromTapResearch API
+CampaignsJob.perform_later
